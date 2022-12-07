053880777a2ac9b9913783dfdc9bcdf7ba261f2d
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-12-07 19:42:36 +0800

    Implemented methods for existing tests in EmployeeInfo Class

--- LinqExamples/EmployeeInfo.cs
+++ LinqExamples/EmployeeInfo.cs
@@ -4,12 +4,20 @@
     {
         public static IEnumerable<Employee> EmployeesWithSalaryGreaterThan100K(IEnumerable<Employee> employees)
         {
+            employees = from emp in employees
+                        where emp.Salary > 100000
+                        select emp;
             return employees;
         }
 
         public static double AverageConsultantSalaryInBrisbane(IEnumerable<Employee> employees)
         {
-            return 0;
+            var employeesInBrisbane = from emp in employees
+                                      where emp.City == "Brisbane" && emp.Level == "Consultant"
+                                      select emp;
+            var avgBrisbaneConsultantSalary = EmployeeInfoHelper.AverageSalary(employeesInBrisbane);
+
+            return avgBrisbaneConsultantSalary;
         }
     }
 }
