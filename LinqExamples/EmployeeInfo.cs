e78bcaea49cafde5b6f7d758198a55475f80aa46
rwang031995 <75116221+rwang031995@users.noreply.github.com>
2022-12-08 21:20:29 +1000

    Implemented the following methods in EmployeeInfo.cs: EmployeesWithSalaryGreaterThan100K() - Tested, AverageConsultantSalaryInBrisbane() - Tested.

--- LinqExamples/EmployeeInfo.cs
+++ LinqExamples/EmployeeInfo.cs
@@ -4,12 +4,14 @@
     {
         public static IEnumerable<Employee> EmployeesWithSalaryGreaterThan100K(IEnumerable<Employee> employees)
         {
-            return employees;
+            // Filter employees by salary > 100,000
+            return employees.Where(employee => employee.Salary > 100000);
         }
 
         public static double AverageConsultantSalaryInBrisbane(IEnumerable<Employee> employees)
         {
-            return 0;
+            // Average Salary of employees filtered by location and level to be Brisbane and Consultant respectively.
+            return employees.Where(employee => employee.City == "Brisbane" && employee.Level == "Consultant").AverageSalary();
         }
     }
 }
